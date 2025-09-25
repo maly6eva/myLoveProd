7e68e624c340d2ab3b05bf9e9c19569184eef3ca
Malysheva Ksenia <maly6eva.ksenia@gmail.com>
2025-09-25 16:46:14 +0300

    refactor

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,9 +1,9 @@
-import { useSelector} from "react-redux";
-import type { RootState } from '../../redux/redux-store.ts'
+import {useSelector} from "react-redux";
+import type {RootState} from '../../redux/redux-store.ts'
 import s from './Users.module.css'
 import {follow, setusers, unfollow} from '../../redux/users-reducer'
-import { useDispatch } from 'react-redux'
-import type { AppDispatch } from '../../redux/redux-store'
+import {useDispatch} from 'react-redux'
+import type {AppDispatch} from '../../redux/redux-store'
 import axios from "axios";
 import {useEffect} from "react";
 
@@ -28,15 +28,15 @@ type UsersAPIPropsTypes = {
 }
 
 
-
 export const Users = () => {
     const dispatch = useDispatch<AppDispatch>()
-    const  usersPages = useSelector((state: RootState) => state.usersPages)
+    const usersPages = useSelector((state: RootState) => state.usersPages)
 
     useEffect(() => {
-        if(usersPages.usersData.length === 0) {
-            axios.get<UsersAPIPropsTypes>("/api/users")
-                .then((response) => {
+        if (usersPages.usersData.length === 0) {
+            const fetchUsers = async () => {
+                try {
+                    const response = await axios.get<UsersAPIPropsTypes>("/api/users")
                     const mappedUsers = response.data.items.map((u) => ({
                         id: u.id,
                         followed: u.followed,
@@ -49,12 +49,16 @@ export const Users = () => {
                         }
                     }))
                     dispatch(setusers(mappedUsers))
-                })
-                .catch(err => console.error("Ошибка при загрузке пользователей:", err))
+                } catch (err) {
+                    console.error("Ошибка при загрузке пользователей:", err)
+                }
+            }
+            fetchUsers()
         }
     }, [dispatch, usersPages.usersData.length])
 
-    return(
+
+    return (
         <div>
             {usersPages.usersData.map((u) => {
                 return (
@@ -65,8 +69,8 @@ export const Users = () => {
                           </div>
                             <div>
                                 {u.followed
-                                    ?  <button onClick={() => dispatch(unfollow(u.id))}>Unfollow</button>
-                                :  <button onClick={() => dispatch(follow(u.id))}>Follow</button>}
+                                    ? <button onClick={() => dispatch(unfollow(u.id))}>Unfollow</button>
+                                    : <button onClick={() => dispatch(follow(u.id))}>Follow</button>}
 
                             </div>
                         </span>
